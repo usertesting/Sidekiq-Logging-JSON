4c981eaf1ed2538e4ffc41fc5c9e94c03e0f35db
Joshua Delsman <j@srv.im>
2017-06-30 09:46:05 -0700

    Don't prefix keys with @

--- lib/sidekiq/logging/json.rb
+++ lib/sidekiq/logging/json.rb
@@ -9,35 +9,35 @@ module Sidekiq
         # Provide a call() method that returns the formatted message.
         def call(severity, time, program_name, message)
           {
-            '@timestamp' => time.utc.iso8601,
-            '@fields' => {
+            'timestamp' => time.utc.iso8601,
+            'fields' => {
               :pid => ::Process.pid,
               :tid => "TID-#{Thread.current.object_id.to_s(36)}",
               :context => "#{context}",
               :program_name => program_name,
               :worker => "#{context}".split(" ")[0]
             },
-            '@type' => 'sidekiq',
-            '@status' => nil,
-            '@severity' => severity,
-            '@run_time' => nil,
+            'type' => 'sidekiq',
+            'status' => nil,
+            'severity' => severity,
+            'run_time' => nil,
           }.merge(process_message(message)).to_json + "\n"
         end
 
         def process_message(message)
           case message
           when Exception
-            { '@status' => 'exception', '@message' => message.message }
+            { 'status' => 'exception', 'message' => message.message }
           when Hash
             if message["retry"]
               {
-                '@status' => 'retry',
-                '@message' => "#{message['class']} failed, retrying with args #{message['args']}."
+                'status' => 'retry',
+                'message' => "#{message['class']} failed, retrying with args #{message['args']}."
               }
             else
               {
-                '@status' => 'dead',
-                '@message' => "#{message['class']} failed with args #{message['args']}, not retrying."
+                'status' => 'dead',
+                'message' => "#{message['class']} failed with args #{message['args']}, not retrying."
               }
             end
           else
@@ -45,9 +45,9 @@ module Sidekiq
             status = result[0].match(/^(start|done|fail):?$/) || []
 
             {
-              '@status' => status[1],                                   # start or done
-              '@run_time' => status[1] && result[1] && result[1].to_f,  # run time in seconds
-              '@message' => message
+              'status' => status[1],                                   # start or done
+              'run_time' => status[1] && result[1] && result[1].to_f,  # run time in seconds
+              'message' => message
             }
           end
         end
